d746e7676ca3ac8bf06feff0df26ba832bbd38e2
MrcBey <meric.renksec@outlook.de>
2019-05-13 11:31:14 +0200

    Änderung von Meric auf Ayo's Branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("das ist nur eine Änderung mal schauen ob das klappt");
+        System.out.println("von Meric");
     }
 }
